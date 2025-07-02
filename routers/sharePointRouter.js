dcef14e5b44d08305dd8d68d7433fe87a32142fe
Noor Ali <shaikhnoorali92@gmail.com>
2025-07-03 04:15:22 +0530

    fix: Correct import path for SharePointController in sharePointRouter

--- routers/sharePointRouter.js
+++ routers/sharePointRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { scanSharePoint } from '../controllers/sharePointController.js';
+import { scanSharePoint } from '../controllers/SharePointController.js';
 const router=express.Router();
 router.post('/sharepoint-scanner', scanSharePoint);
 
