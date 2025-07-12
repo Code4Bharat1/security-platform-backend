1731c629bb213cf5c5db0700f66b84555114a134
Noor Ali <shaikhnoorali92@gmail.com>
2025-07-12 05:41:03 +0530

    feat: Refactor server.js to organize route imports and enhance readability

--- server.js
+++ server.js
@@ -3,70 +3,139 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 import express from 'express';
+
 import cors from 'cors';
+
 import mongoose from 'mongoose';
+
 import connectDB from './utils/db.js';
 
 // Tool Routes
+
 import dnsRoutes from './routers/dnsRouter.js';
+
 import wafRoutes from './routers/wafRouter.js';
+
 import scanRoutes from './routers/scanRouter.js';
+
 import sharePointRoutes from './routers/sharePointRouter.js';
+
 import wordpressRoutes from './routers/wordpressRouter.js';
+
 import sitemapRoutes from './routers/sitemapRouter.js';
+
 import brokenlinkRoutes from './routers/brokenLinkRouter.js';
+
 import keywordRoutes from './routers/keyword.router.js';
+
 import speedRoutes from './routers/speed.router.js';
 import metaAnalyzeRoutes from './routers/metaAnalyzeRoutes.js';
+
+
 import oauthTokenRoutes from './routers/oauth.router.js';
+
 import jwtRoutes from './routers/jwtRoutes.js';
+
 import jwtsignatureRoutes from './routers/jwtsignature.routes.js';
+
 import IPGeoRouter from './routers/ipgeo.router.js';
+
 import clickjackingRouter from './routers/clickjacking.router.js';
+
 import httpsRouter from './routers/https.router.js';
+
 import portScannerRouter from './routers/portScanner.router.js';
+
 import asnLookupRouter from './routers/asnlookup.router.js';
+
 import mochaRoutes from './routers/mochaTest.routes.js';
+
 import reverseDNSRoutes from './routers/reverseDNSRoutes.js';
+
 import csrfRoutes from './routers/csrfRoutes.js';
+
 import regexRoutes from './routers/regexRoutes.js';
+
+
 import sessionFixationRoutes from './routers/sessionFixationRoutes.js';
+
 import whoisRoutes from './routers/whoisRouter.js';
+
 import subdomainRoutes from './routers/subdomainRouter.js';
+
 import xssTesterRoutes from './routers/xssTesterRouter.js';
+
 import secretScanRoutes from './routers/secretScanRouter.js';
+
 import openRedirectRoutes from './routers/openRedirectRouter.js';
+
 import codeObfuscationRoutes from './routers/codeObfuscationRouter.js';
+
 import analysisRoutes from './routers/analysisRouter.js';
+
 import sonarRoutes from './routers/sonarRouter.js';
+
 import analyzeCodeRoutes from './routers/analyzeCodeRouter.js';
+
 import apiTestRoutes from './routers/apiTestRouter.js';
+
 import fingerprintRoutes from './routers/fingerprintRouter.js';
+
 import bruteForceRoutes from './routers/bruteForceRouter.js';
+
 import brokenAccessRoutes from './routers/brokenAccessRouter.js';
+
+
 import ssrfRoutes from './routers/ssrfRouter.js';
+
 import sensitiveFileRoutes from './routers/sensitiveFileRouter.js';
+
 import aiRoutes from './routers/aiRouter.js';
+
 import aiHeaderRoutes from './routers/aiHeaderRouter.js';
+
 import rogueWiFiRoutes from './routers/rogueWifiRoutes.js';
+
+
 import linkDetectorRoutes from './routers/linkDetectorRouter.js';
+
 import sqliScannerRoutes from './routers/sqliScannerRouter.js';
+
 import hashGeneratorRouter from './routers/hashGeneratorRouter.js';
+
+
 import johnRoutes from "./routers/johnRouter.js";
+
 import secureCryptRoutes from "./routers/secureCryptRouter.js";
+
 import nexposeRoutes from './routers/nexposeRouter.js';
+
 import mdrMonitorRouter from "./routers/mdrMonitorRouter.js";
+
+
 import folderThreatScanRoutes from './routers/folderThreatScanRouter.js';
+
 import usbScannerRoutes from './routers/usbScannerRoutes.js';
+
 import dataLeakRoutes from './routers/dataLeakRoutes.js'; 
+
 import socialPrivacyRoutes from "./routers/socialPrivacyRoutes.js";
+
 import fakeSoftwareRoutes from "./routers/fakeSoftwareRouter.js";
+
+
 import whatsappPrivacyRoutes from './routers/whatsappPrivacyRouter.js';
+
 import emailAttachmentRoutes from "./routers/emailAttachmentRouter.js";
+
 import ipInfoRouter from './routers/ipInfoRouter.js';
+
 import thirdPartyPermissionRoutes from "./routers/thirdPartyPermissionRouter.js";
+
 import portActivityRouter from "./routers/portActivityRouter.js";
+
 import qrRoutes from './routers/qrRoute.js'; 
 
 
@@ -90,65 +159,133 @@ app.use((req, res, next) => {
 await connectDB(); // ✅ uses mongoose.connect() inside utils/db.js
 
 // API Routes Mounting
+
 app.use('/api/keyword', keywordRoutes);
+
 app.use('/api/speed', speedRoutes);
+
 app.use('/api/meta', metaAnalyzeRoutes);
+
 app.use('/api/auth', oauthTokenRoutes);
+
 app.use('/api/jwt', jwtRoutes);
+
 app.use('/api/jwtsign', jwtsignatureRoutes);
+
 app.use('/api/ipgeo', IPGeoRouter);
+
+
+
+
+
 app.use('/api/clickjacking', clickjackingRouter);
+
 app.use('/api/http', httpsRouter);
+
 app.use('/api/port', portScannerRouter);
+
 app.use('/api/asnLookup', asnLookupRouter);
+
 app.use('/api/mocha', mochaRoutes);
+
 app.use('/api/reverse', reverseDNSRoutes);
+
 app.use('/api/csrf', csrfRoutes);
+
 app.use('/api/regex', regexRoutes);
+
 app.use('/api/session', sessionFixationRoutes);
+
 app.use('/api/dns', dnsRoutes);
+
 app.use('/api/waf', wafRoutes);
+
 app.use('/api/scan', scanRoutes);
+
 app.use('/api/sharepoint', sharePointRoutes);
+
 app.use('/api/wordpress', wordpressRoutes);
+
 app.use('/api/sitemap', sitemapRoutes);
+
 app.use('/api/brokenlink', brokenlinkRoutes);
+
 app.use('/api/whois', whoisRoutes);
+
 app.use('/api/subdomain', subdomainRoutes);
+
 app.use('/api/xssTester', xssTesterRoutes);
+
 app.use('/api/secretKeyScanner', secretScanRoutes);
+
 app.use('/api/openRedirectTester', openRedirectRoutes);
+
 app.use('/api/code', codeObfuscationRoutes);
+
+
 app.use('/api/analysis', analysisRoutes);
+
 app.use('/api/sonar', sonarRoutes);
+
 app.use('/api/analyze', analyzeCodeRoutes);
+
 app.use('/api/apiTest', apiTestRoutes);
+
 app.use('/api/fingerprint', fingerprintRoutes);
+
 app.use('/api/bruteForce', bruteForceRoutes);
+
+
 app.use('/api/brokenAccess', brokenAccessRoutes);
+
 app.use('/api/ssrf', ssrfRoutes);
+
 app.use('/api/sensitiveFile', sensitiveFileRoutes);
+
 app.use('/api/ai', aiRoutes);
+
 app.use('/api/aiHeader', aiHeaderRoutes);
+
 app.use('/api/rogue-wifi', rogueWiFiRoutes);
+
 app.use('/api/link-detector', linkDetectorRoutes);
+
 app.use('/api/sqli', sqliScannerRoutes);
+
 app.use('/api', hashGeneratorRouter);
+
 app.use('/api/john-the-ripper', johnRoutes);
+
 app.use("/api/securecrypt", secureCryptRoutes); 
+
 app.use('/api/nexpose', nexposeRoutes);
+
 app.use("/api/mdr-monitor", mdrMonitorRouter);
+
 app.use("/api/folder-scan", folderThreatScanRoutes); 
+
 app.use('/api/usb-scan', usbScannerRoutes);
+
 app.use("/api/data-leak", dataLeakRoutes);
+
 app.use("/api", socialPrivacyRoutes);  
+
 app.use("/api", fakeSoftwareRoutes);
+
 app.use("/api/whatsapp-privacy", whatsappPrivacyRoutes);
+
 app.use("/api/email-attachment", emailAttachmentRoutes); 
+
 app.use('/api/ip-info', ipInfoRouter);
+
 app.use("/api", thirdPartyPermissionRoutes);
+
 app.use("/api", portActivityRouter);
+
 app.use('/api/qr', qrRoutes);
+
+
+
 app.use('/uploads', express.static('uploads'));
 
 
