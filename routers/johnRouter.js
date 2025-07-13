5a87c216cb872817fd2b8dea2ba655850a5cff87
Noor Ali <shaikhnoorali92@gmail.com>
2025-07-13 05:52:53 +0530

    haan yaar

--- routers/johnRouter.js
+++
@@ -1,9 +0,0 @@
-// routers/johnRouter.js
-import express from "express";
-import { crackHash } from "../controllers/johnController.js";
-
-const router = express.Router();
-
-router.post("/", crackHash); // ✅ Make sure this is POST
-
-export default router;
